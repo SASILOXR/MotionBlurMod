5e9044af0b7fa04e5a6eb024585afbe8b0e0811a
sasiloxr <sasiloxr2@gmail.com>
2025-01-16 17:58:49 +0800

    finsh config load and save

--- src/main/java/com/sasiloxr/motionblur/MotionBlurMod.java
+++ src/main/java/com/sasiloxr/motionblur/MotionBlurMod.java
@@ -4,6 +4,7 @@ import com.sasiloxr.motionblur.command.CommandMotionBlur;
 import com.sasiloxr.motionblur.resource.MotionBlurPostManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.SimpleReloadableResourceManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
@@ -22,9 +23,9 @@ import java.util.Map;
 
 @Mod(modid = MotionBlurMod.MODID, name = MotionBlurMod.NAME, version = MotionBlurMod.VERSION, acceptedMinecraftVersions = "1.8.9")
 public class MotionBlurMod {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "NewMotionBlur";
     public static final String VERSION = "1.0";
-    public static final String NAME = "MotionBlur";
+    public static final String NAME = "NewMotionBlur";
     private Map domainResourceManager;
     Minecraft mc = Minecraft.getMinecraft();
     public boolean isNew = true;
@@ -32,7 +33,7 @@ public class MotionBlurMod {
     public boolean enabled = false;
     private File configFile;
     private Configuration config;
-    private static boolean init;
+    private static boolean firstLoad = true;
 
 
     @Mod.Instance(MotionBlurMod.MODID)
@@ -48,7 +49,7 @@ public class MotionBlurMod {
         ClientCommandHandler.instance.registerCommand(new CommandMotionBlur());
         config = new Configuration(this.configFile);
         loadConfig();
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(INSTANCE);
     }
 
     @EventHandler
@@ -67,7 +68,7 @@ public class MotionBlurMod {
 
     private void updateConfig(Configuration config, boolean load) {
         Property prop1 = config.get("MotionBlur", "isNew", true);
-        Property prop2 = config.get("MotionBlur", "blurMount", 0);
+        Property prop2 = config.get("MotionBlur", "blurMount", 0.0);
         Property prop3 = config.get("MotionBlur", "enabled", false);
         if (!load) {
             prop3.setValue(INSTANCE.enabled);
@@ -75,27 +76,36 @@ public class MotionBlurMod {
             prop1.setValue(INSTANCE.isNew);
         } else {
             INSTANCE.enabled = prop3.getBoolean();
-            INSTANCE.blurMount = prop2.getInt();
+            INSTANCE.blurMount = prop2.getDouble();
             INSTANCE.isNew = prop1.getBoolean();
         }
     }
 
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent event) {
-        if (domainResourceManager == null) {
-            try {
-                for (Field field : SimpleReloadableResourceManager.class.getDeclaredFields()) {
-                    if (field.getType() != Map.class) continue;
-                    field.setAccessible(true);
-                    this.domainResourceManager = (Map) field.get(mc.getResourceManager());
-                    break;
+        if (event.phase == TickEvent.Phase.END) {
+            if (domainResourceManager == null) {
+                try {
+                    for (Field field : SimpleReloadableResourceManager.class.getDeclaredFields()) {
+                        if (field.getType() != Map.class) continue;
+                        field.setAccessible(true);
+                        this.domainResourceManager = (Map) field.get(mc.getResourceManager());
+                        break;
+                    }
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
             }
-        }
-        if (!this.domainResourceManager.containsKey("motionblur")) {
-            this.domainResourceManager.put("motionblur", new MotionBlurPostManager());
+            if (!this.domainResourceManager.containsKey("motionblur")) {
+                this.domainResourceManager.put("motionblur", new MotionBlurPostManager());
+                System.out.println("add resource");
+            }
+
+            if (MotionBlurMod.INSTANCE.enabled && !mc.entityRenderer.isShaderActive() && mc.inGameHasFocus && firstLoad) {
+                mc.entityRenderer.setupOverlayRendering();
+                mc.entityRenderer.loadShader(new ResourceLocation("motionblur", "motionblur"));
+                firstLoad = false;
+            }
         }
 
     }
